a76be0f0490216d7b0c93ee820abdf23f0bd03df
vipin-thomas <bewithvipin@gmail.com>
2025-03-29 00:20:46 +0530

    Test deployment via GitHub Actions

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! This is a simple Node.js application.');
+  res.send('Hello, World! 🚀 Deployed from GitHub Actions!');
 });
 
 app.listen(PORT, () => {
